70310f449f6b9a94d43e308d5001ec29c3a1f32f
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-08-17 14:08:38 +0300

    feat: add hide sidebar feature on screen size <=900px

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,13 @@ import { useStateContext } from "../contexts/ContextProvider";
 import { links } from "../data/dummy";
 
 function Sidebar() {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const normalLink =
     " flex m-2 items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray ";
@@ -52,7 +58,7 @@ function Sidebar() {
                     <NavLink
                       to={`/${link.name} `}
                       key={link.name}
-                      onClick={() => {}}
+                      onClick={handleCloseSideBar}
                       className={({ isActive }) =>
                         isActive ? activeLink : normalLink
                       }
